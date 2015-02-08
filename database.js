369eb24b108db2fec590b796917822cd0efb1e95
Pipsa <pia.narvanen@gmail.com>
2015-02-08 09:51:07 +0200

    basic functionality

---
+++ database.js
@@ -0,0 +1,33 @@
+"use strict"
+
+storage.storage = function () {
+	var status = 0;
+
+
+	var import = function (var source) {
+		// No functionality yet
+		console.log("storage.import()");
+		status = 1;
+		return (status);
+	};
+
+	var add = function (var info)
+	// No functionality yet
+		console.log("storage.add()");
+		status = 1;
+		return (status);
+	};
+
+	var get (var key) {
+		// No functionality yet
+		console.log("storage.get()");
+		status = 1;
+		return (info);
+	};
+
+ 	return {
+ 		import : import,
+ 		add : add,
+ 		get : get
+ 	};
+};
